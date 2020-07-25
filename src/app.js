97afd2d92dbd3c5be05190bedeefccc89e37e8ac
joaovitors1g <joao_vitor9846@hotmail.com>
2020-07-25 15:42:23 -0300

    feat: add app functionalities
    This commit adds all the app logic, so tests can pass.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
+
+const githubRegex = /http(s)?:\/\/github\.com\/(\w+)\/(\w+)/;
 
 const app = express();
 
@@ -10,24 +12,100 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+app.get('/repositories', (request, response) => {
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post('/repositories', (request, response) => {
+  const { title, url, techs } = request.body;
+
+  if (!githubRegex.test(url)) {
+    return response.status(400).json({
+      message: "'url' is not a github repository",
+    });
+  }
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put('/repositories/:id', (request, response) => {
+  const repositoryIndex = repositories.findIndex(
+    (repo) => repo.id === request.params.id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      message: 'Repository not found',
+    });
+  }
+
+  const repository = repositories[repositoryIndex];
+
+  const { title, url, techs } = request.body;
+
+  if (title) {
+    repository.title = title;
+  }
+
+  if (url && githubRegex.test(url)) {
+    repository.url = url;
+  }
+
+  if (techs && Array.isArray(techs) && techs.length > 0) {
+    repository.techs = techs;
+  }
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', (request, response) => {
+  const repositoryIndex = repositories.findIndex(
+    (repo) => repo.id === request.params.id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      message: 'Repository not found',
+    });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post('/repositories/:id/like', (request, response) => {
+  const repositoryIndex = repositories.findIndex(
+    (repo) => repo.id === request.params.id
+  );
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({
+      message: 'Repository not found',
+    });
+  }
+
+  const repository = repositories[repositoryIndex];
+
+  repository.likes += 1;
+
+  repositories[repositoryIndex] = repository;
+
+  return response.json({
+    likes: repository.likes,
+  });
 });
 
 module.exports = app;
